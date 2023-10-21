8b614d2d01983b3a71717726e4d620381e492f68
SergeyAverin <ct.averin@yandex.ru>
2023-10-21 15:56:25 +0600

    feat(frontend): added main router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 
+import MainRouter from "./routers";
+import { BrowserRouter } from "react-router-dom";
+
 
 const App: React.FC = () => {
   return (
     <div>
-        app
+      <BrowserRouter>
+          <MainRouter />
+      </BrowserRouter>
     </div>
   );
 };
