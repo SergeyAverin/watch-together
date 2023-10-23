28b2d2fb2fe2e587d399010f863c861a5c24588f
SergeyAverin <ct.averin@yandex.ru>
2023-10-23 17:36:47 +0600

    feat(backend): added player web socket

--- backend/src/routers/main.py
+++ backend/src/routers/main.py
@@ -1,8 +1,42 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 
 main_router = APIRouter()
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
+
 @main_router.get("/")
 async def root():
+    ''''''
     return {"message": "Hello World"}
+
+
+@main_router.websocket('/player')
+async def player(websocket: WebSocket):
+    ''''''
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            #await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(data)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast({"event": "error"})
