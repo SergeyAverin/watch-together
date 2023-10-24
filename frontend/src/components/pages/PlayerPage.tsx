5da55f549110c3c3719530c995abe1b5b92a449c
SergeyAverin <ct.averin@yandex.ru>
2023-10-24 22:18:14 +0600

    fix(frontend): fixed styles

--- frontend/src/components/pages/PlayerPage.tsx
+++ frontend/src/components/pages/PlayerPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { Center } from "@atoms/index";
 import { VideoPlayer } from "@organisms/VideoPlayer";
+import { SideBar } from "@organisms/SideBar";
 
 
 const PlayerPage: React.FC = () => {
@@ -9,6 +10,7 @@ const PlayerPage: React.FC = () => {
   return (
     <Center>
       <div>
+        <SideBar />
         <VideoPlayer />
       </div>
     </Center>
