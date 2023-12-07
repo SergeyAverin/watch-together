b144bedeb1dc3e59d83c1caac7187727d82356f8
SergeyAverin <ct.averin@yandex.ru>
2023-12-07 23:18:58 +0600

    style(frontend): formatted full screen button

--- frontend/src/components/molecules/FullScreenButton/FullScreenButtons.tsx
+++ frontend/src/components/molecules/FullScreenButton/FullScreenButtons.tsx
@@ -1,20 +1,18 @@
-import { IconButton } from '@atoms/index'
-import React from 'react'
+import { IconButton } from "@atoms/index";
+import React from "react";
 
 import FullScreenIcon from "@public/FullScreenIcon.svg";
 
-
 interface IFullScreenButtonProps {
-    openFullScreen: Function
+  openFullScreen: Function;
 }
 
-export const FullScreenButton: React.FC<IFullScreenButtonProps> = ({ openFullScreen }) => {
-    return (
-        <>
-            <IconButton
-                icon={<FullScreenIcon />}
-                clickFunction={openFullScreen}
-            />
-        </>
-    )
-}
+export const FullScreenButton: React.FC<IFullScreenButtonProps> = ({
+  openFullScreen,
+}) => {
+  return (
+    <>
+      <IconButton icon={<FullScreenIcon />} clickFunction={openFullScreen} />
+    </>
+  );
+};
