0c20ac64b45d5383f971cea522c9371fa54ad348
SergeyAverin <ct.averin@yandex.ru>
2023-12-07 23:15:46 +0600

    perf(frontend): separated full screen button from video player

---
+++ frontend/src/components/molecules/FullScreenButton/FullScreenButtons.tsx
@@ -0,0 +1,20 @@
+import { IconButton } from '@atoms/index'
+import React from 'react'
+
+import FullScreenIcon from "@public/FullScreenIcon.svg";
+
+
+interface IFullScreenButtonProps {
+    openFullScreen: Function
+}
+
+export const FullScreenButton: React.FC<IFullScreenButtonProps> = ({ openFullScreen }) => {
+    return (
+        <>
+            <IconButton
+                icon={<FullScreenIcon />}
+                clickFunction={openFullScreen}
+            />
+        </>
+    )
+}
