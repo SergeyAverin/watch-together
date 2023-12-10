5b2e4506c43d1ef729f198ed4ab6ce0256d91be0
SergeyAverin <ct.averin@yandex.ru>
2023-12-10 16:30:59 +0600

    feat(frontend): added user list slice in user count component

--- frontend/src/components/molecules/UserCount/UserCount.tsx
+++ frontend/src/components/molecules/UserCount/UserCount.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
 import { Flex, IconButton } from "@atoms/index";
+import { useSelector } from "react-redux";
 
-
+import { usersCountSelector } from "@redux/selectors/usersListSelectores";
 import UserCountIcon from '@public/UserCountIcon.svg'
 
 
 interface IUserCountProps {
-    userCount: number
 }
 
-export const UserCount: React.FC<IUserCountProps> = ({ userCount }) => {
+export const UserCount: React.FC<IUserCountProps> = () => {
+    const userCount = useSelector(usersCountSelector)
     return (
         <Flex alignItems="center" justifyContent="center" flexDirection="column">
             <IconButton icon={<UserCountIcon />} clickFunction={() => {}} />
