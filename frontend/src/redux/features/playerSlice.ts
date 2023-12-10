68cfb84ee2862a26f058a40982358481ced4133c
SergeyAverin <ct.averin@yandex.ru>
2023-12-10 16:24:52 +0600

    feat(frontend): added base video state in player slice

--- frontend/src/redux/features/playerSlice.ts
+++ frontend/src/redux/features/playerSlice.ts
@@ -3,20 +3,46 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 
 interface PlayerState {
   isPaused: boolean;
+  currentTime: number;
+  duration: number;
+  source: string | null;
+  isInteracted: boolean;
 }
 
 const initialState: PlayerState = {
   isPaused: false,
+  currentTime: 0,
+  duration: 0,
+  source: null,
+  isInteracted: false,
 };
 
 export const playerSlice = createSlice({
   name: "player",
   initialState,
   reducers: {
-    toggle: (state) => {
-      state.isPaused = !state.isPaused;
+    setIsPaused: (state, actions: PayloadAction<boolean>) => {
+      state.isPaused = actions.payload;
+    },
+    setVideoSource: (state, action: PayloadAction<string>) => {
+      state.source = action.payload;
+    },
+    setCurrentTime: (state, action: PayloadAction<number>) => {
+      state.currentTime = action.payload;
+    },
+    setDuration: (state, action: PayloadAction<number>) => {
+      state.duration = action.payload;
+    },
+    setIsInteracted: (state, action: PayloadAction<boolean>) => {
+      state.isInteracted = action.payload;
     },
   },
 });
 
-export const { toggle } = playerSlice.actions;
+export const {
+  setIsPaused,
+  setVideoSource,
+  setCurrentTime,
+  setDuration,
+  setIsInteracted,
+} = playerSlice.actions;
