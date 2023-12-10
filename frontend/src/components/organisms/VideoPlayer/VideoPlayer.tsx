8bb385146680360a233cbeb01c37f915f82ef55f
SergeyAverin <ct.averin@yandex.ru>
2023-12-10 16:51:24 +0600

    feat(frontend): added video player component

--- frontend/src/components/organisms/VideoPlayer/VideoPlayer.tsx
+++ frontend/src/components/organisms/VideoPlayer/VideoPlayer.tsx
@@ -1,175 +1,86 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 
-import { Flex, Margin, IconButton } from "@atoms/index";
-import { formatVideoTime } from "@utils/formatTime";
+import { Flex, Margin } from "@atoms/index";
 import { VideoProgressBar } from "@molecules/VideoProgressBar";
-import { VolumeProgressBar } from "@molecules/VolumeProgressBar/VolumeProgressBar"
-import { VideoSpeed } from "@molecules/VideoSpeed";
+import { VolumeProgressBar } from "@molecules/VolumeProgressBar/VolumeProgressBar";
+import { TimeWidget } from "@molecules/TimeWidget/TimeWidget";
+import { RewindButtons } from "@molecules/RewindButtons/RewindButtons";
+import { FullScreenButton } from "@molecules/FullScreenButton/FullScreenButtons";
+import { Video } from "@molecules/Video/Video";
+import { PlayButton } from "@molecules/PlayButton/PlayButton";
+import { useAppSelector, useAppDispatch } from "@hooks/storeHooks";
+import { useVideoPlayerSocket } from "@hooks/useVideoPlayerWebSocket";
+import { useVideoOperations } from "@hooks/useVideoOperations";
+import {
+  currentTimeSelector,
+  durationSelector,
+  isPausedSelector
+} from "@redux/selectors/playerSelectores";
+import { setIsInteracted } from "@redux/features/playerSlice";
+import { stringifyMessage } from "@utils/socketMessaeg";
 
-import RewindLeft from '@public/RewindLeft.svg'
-import RewindRight from '@public/RewindRight.svg'
-import FullScreenIcon from '@public/FullScreenIcon.svg'
-import PauseIcon from '@public/PauseIcon.svg'
-import PlayIcon from '@public/PlayIcon.svg'
-
-
-import "./VideoPlayer.sass";
 
 const TEST_VIDEO_URL = process.env.TEST_VIDEO_URL as string;
-const BACKEND_HOST = process.env.BACKEND_HOST as string;
-const BACKEND_PORT = process.env.BACKEND_PORT as string;
 
+interface IVideoPlayerProps {}
 
-interface IVideoPlayerProps {
-  setUsers: Function
-}
-
-export const VideoPlayer: React.FC<IVideoPlayerProps> = ({setUsers}) => {
+export const VideoPlayer: React.FC<IVideoPlayerProps> = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
-  const [isPaused, setIsPaused] = useState<boolean | undefined>(false);
-  const [videoNode, setVideoNode] = useState(videoRef.current);
-  const [duration, setDuration] = useState<number | null>(null);
-  const [currentTime, setCurrentTime] = useState<number | null>(null);
-  const socketRef = useRef<WebSocket|undefined>();
-  const [isTimeWidget, setIsTimeWidget] = useState(true)
   
-  const handleLoadedMetadata = () => {
-    if (videoRef.current) {
-      setDuration(videoRef.current.duration);
-    }
-  };
-
-  const handleTimeUpdate = () => {
-    if (videoRef.current) {
-      setCurrentTime(videoRef.current.currentTime);
-    }
-  };
-
-  useEffect(() => {
-    setVideoNode(videoRef.current);
-    setIsPaused(videoRef.current?.paused);
-    if (videoRef.current) {
-      videoRef.current.muted = false
-    }
-  }, [videoRef]);
+  const dispatch = useAppDispatch()
+  
+  const ws = useVideoPlayerSocket(videoRef)
+  const videoOperations = useVideoOperations(videoRef);
+  
+  const isPaused = useAppSelector(isPausedSelector)
+  const currentTime = useAppSelector(currentTimeSelector);
+  const duration = useAppSelector(durationSelector);
 
 
-  useEffect(() => {
-    socketRef.current = new WebSocket(`ws:/${BACKEND_HOST}:${BACKEND_PORT}/player`);
+  useEffect(()=> {
+    const message = {
+      event: isPaused ? "pause_video" : "'play_video",
+      currentTime: videoRef.current?.currentTime
+    }
+    ws?.send(stringifyMessage(message))      
+  }, [isPaused])
 
-    socketRef.current.onopen = () => {
-      console.log('WebSocket connected');
-    };
-
-    socketRef.current.onmessage = (event) => {
-      const message = JSON.parse(event.data)
-      setUsers(message.usersCount)
-      if (message.event == 'play' && videoNode) {
-        videoNode?.play()
-      }
-      if (message.event == 'pause' && videoNode) {
-        videoNode?.pause()
-      }
-      if (videoNode) {
-        videoNode.currentTime = message.currentTime
-      }
-    };
-
-    return () => {
-      if(socketRef.current) {
-        socketRef.current.close();
-      }
-    };
-  }, [videoNode]);
-
-
-  const onPause = () => {
-    if (socketRef.current) {
-      socketRef.current.send(`{"event": "pause", "currentTime": "${videoNode?.currentTime}"}`)
-    }
-  }
-  const onPlay = () => {
-    if (socketRef.current) {
-      socketRef.current.send(`{"event": "play", "currentTime": "${videoNode?.currentTime}"}`)
-    }
-  }
-  const openFullScreen = () => {
-    if (videoNode) {
-      videoNode.requestFullscreen()
-    }
-  }
-  const rewind = (second: number) => {
-    const isPaused = videoNode?.paused
-      if (videoNode) {
-        videoNode.pause();
-        videoNode.currentTime = videoNode.currentTime + second; 
-        if (!isPaused) {
-          videoNode.play();
-        }
-    }
-  }
-  const playVideo = () => {
-    if (videoNode) {
-      videoNode.play();
-    }
-  }
-  const pauseVideo = () => {
-    if (videoNode) {
-      videoNode.pause();
-    }
-  }
-  return (  
+  return (
     <>
-      <video
-        className="video-player"
-        ref={videoRef}
-        onLoadedMetadata={handleLoadedMetadata}
-        onTimeUpdate={handleTimeUpdate}
-        onPause={onPause}
-        onPlay={onPlay}
-        autoPlay={true}
-      >
-        <source src={TEST_VIDEO_URL} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <Video
+        handleLoadedMetadata={videoOperations.handleLoadedMetadata}
+        handleTimeUpdate={videoOperations.handleTimeUpdate}
+        videoPause={videoOperations.videoPause}
+        videoPlay={videoOperations.videoPlay}
+        src={TEST_VIDEO_URL}
+        videoRef={videoRef}
+      />
       <Margin marginTop="30px">
         <Margin marginBottom="20px">
-            { videoRef.current  && duration && <VideoProgressBar video={videoNode} /> }
+          <VideoProgressBar video={videoRef.current} />
         </Margin>
+
         <Flex alignItems="flex-start" justifyContent="space-between">
           <Flex alignItems="center" justifyContent="flex-start">
-          {videoRef.current?.paused ? (
-            <IconButton  clickFunction={() => playVideo()} icon={<PlayIcon />} />
-          ) : (
-            <IconButton  clickFunction={() => pauseVideo()} icon={<PauseIcon />} />
-          )}
-          <Margin marginLeft="8px" marginRight="8px">
-              { videoRef.current  && duration && <VolumeProgressBar video={videoNode} progress={0.5} /> }
-          </Margin>
-          <div className="video-player__time" onClick={() => setIsTimeWidget((prev) => !prev)}>
-            {isTimeWidget ? 
-              <>
-                {duration && formatVideoTime(currentTime as number)} /{" "}
-                {duration && formatVideoTime(duration as number)}
-              </>
-              :
-              <>
-                -{duration && currentTime && formatVideoTime((duration - currentTime) as number)} /{" "}
-                {duration && formatVideoTime(duration as number)}
-              </>
-            }
-          </div>
+            <PlayButton
+              videoPause={videoOperations.videoPause}
+              videoPlay={videoOperations.videoPlay}
+            />
+
+            <Margin marginLeft="8px" marginRight="8px">
+              <TimeWidget currentTime={currentTime} duration={duration} />
+            </Margin>
+
+            <Margin marginLeft="8px" marginRight="8px">
+              <VolumeProgressBar video={videoRef.current} progress={0.5} />
+            </Margin>
           </Flex>
-            <IconButton icon={<FullScreenIcon />} clickFunction={openFullScreen} />
+
+          <FullScreenButton openFullScreen={videoOperations.openFullScreen} />
         </Flex>
       </Margin>
-      <Margin marginTop="30px">
-        <VideoSpeed  video={videoNode}  />
-      </Margin>
-      <Flex alignItems="flex-start" justifyContent="space-between">
-        <IconButton  clickFunction={() => rewind(-10)} icon={<RewindLeft />} />
-        <IconButton  clickFunction={() => rewind(10)} icon={<RewindRight />} />
-      </Flex>
+      <button onClick={() => dispatch(setIsInteracted(true))}>interact</button>
+      <RewindButtons rewind={videoOperations.rewind} />
     </>
   );
 };
