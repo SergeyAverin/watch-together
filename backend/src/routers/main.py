cf7f85575e1248585769fcd8be5f5fd1910a1965
SergeyAverin <ct.averin@yandex.ru>
2024-01-18 17:43:40 +0600

    feat(backend): added room routes

--- backend/src/routers/main.py
+++ backend/src/routers/main.py
@@ -1,48 +1,25 @@
 import json
+import logging
 
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+from ..sockets.manager import ConnectionManager
 
 
 main_router = APIRouter()
 
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_text(message)
+logger = logging.getLogger()
 
 manager = ConnectionManager()
 
-@main_router.get("/")
-async def root():
-    ''''''
-    return {"active_connections": len(manager.active_connections)}
 
-
-@main_router.websocket('/player')
-async def player(websocket: WebSocket):
-    ''''''
-    await manager.connect(websocket)
+@main_router.websocket('/player/{room_name}')
+async def player_websocket(websocket: WebSocket, room_name: str, user: str):
+    await manager.connect(websocket, room_name, user)
     try:
         while True:
             data = await websocket.receive_text()
             message = json.loads(data)
-            message['usersCount'] = len(manager.active_connections)
-            # message['users'] = manager.active_connections
-            #await manager.send_personal_message(f"You wrote: {data}", websocket)
             await manager.broadcast(json.dumps(message))
     except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast({"event": "error"})
+        await manager.disconnect(websocket, room_name, user)
