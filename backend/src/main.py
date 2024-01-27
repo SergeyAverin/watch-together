46dd145c1dff81a167bb8122b180a1d3a6c8729f
SergeyAverin <ct.averin@yandex.ru>
2024-01-27 16:35:06 +0600

    feat(backend): added socketio app

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from loguru import logger
 
-from .routers.main import main_router
+from .routers.player import player_router
+from .sio import socket_app
+
 
 app = FastAPI()
 
-app.include_router(main_router)
+app.include_router(player_router)
+app.mount("/", socket_app)
+
+
+async def startup_event():
+    logger.info('Server run')
+
+app.add_event_handler("startup", startup_event)
--- backend/src/sockets/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import manager
-from .room_socket_events import RoomSocketEvents
