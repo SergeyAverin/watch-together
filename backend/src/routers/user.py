3c9694047b645764c57d951f98ceb268517c74e2
SergeyAverin <ct.averin@yandex.ru>
2024-02-06 18:58:04 +0600

    feat(backend): added register user router

--- backend/src/routers/user.py
+++ backend/src/routers/user.py
@@ -2,14 +2,23 @@ from fastapi import APIRouter
 
 from ..services.user_service import UserService
 from ..repositories.user_repository import UserRepositorySqlAlchemy
+from ..schemas.user import UserCreateDTO, UserDTO
 
 
 user_router = APIRouter()
 
+user_repository = UserRepositorySqlAlchemy()
+user_service = UserService(user_repository)
+
 
 @user_router.get('/user/{username}')
 def get_user_by_username(username: str):
-    user_repository = UserRepositorySqlAlchemy()
-    user_service = UserService(user_repository)
     user = user_service.get_user_by_username(username)
     return {"username": user}
+
+
+@user_router.post('/user/register')
+def register_user(user_data: UserCreateDTO):
+    user = user_service.register_user(user_data)
+    # user_response = UserDTO.model_validate(user, from_attributes=True)
+    return user
