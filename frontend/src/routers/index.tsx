e5c3b54c4af1fdcd024322401036fb8fce50b164
SergeyAverin <ct.averin@yandex.ru>
2024-03-18 10:44:39 +0600

    feat(frontend): added routing

--- frontend/src/routers/index.tsx
+++ frontend/src/routers/index.tsx
@@ -2,12 +2,16 @@ import React from "react";
 
 import { Route, Routes } from "react-router-dom";
 import PlayerPage from "@pages/PlayerPage";
+import LoginPage from "@pages/LoginPage";
+import NotFoundPage from "@pages/NotFoundPage";
 
 
 const MainRouter: React.FC = () => {
   return (
     <Routes>
-      <Route path="/*" element={<PlayerPage />} />
+      <Route path="/auth/login" element={<LoginPage />} />
+      <Route path="/room/*" element={<PlayerPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
