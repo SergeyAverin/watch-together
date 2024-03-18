9aa8cf630002559d56956eaaf592705bd7ab217a
SergeyAverin <ct.averin@yandex.ru>
2024-03-18 10:43:05 +0600

    feat(frontend): added auth api

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,15 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { authApi } from "./api/authApi";
+
 import { playerSlice } from "./features/playerSlice";
 import { userListSlice } from "./features/userListSlice";
 import { followSLice } from "./features/followSlice";
 
 export const store = configureStore({
   reducer: {
+    [authApi.reducerPath]: authApi.reducer,
     player: playerSlice.reducer,
     usersList: userListSlice.reducer,
     followSLice: followSLice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({}).concat([authApi.middleware]),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
