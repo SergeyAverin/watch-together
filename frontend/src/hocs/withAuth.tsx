b3279c139833a4b0202b0d570579160e9db468c1
SergeyAverin <ct.averin@yandex.ru>
2024-03-18 10:46:19 +0600

    feat(frontend): added with auth hoc

---
+++ frontend/src/hocs/withAuth.tsx
@@ -0,0 +1,18 @@
+import { isCookieExist } from "@utils/cookie";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export function withAuth(WrappedComponent: React.FC) {
+  return function WithAuth() {
+    const navigate = useNavigate();
+    const isExist =isCookieExist('jwt')
+
+    useEffect(() => {
+      if (!isExist) {
+        navigate("/auth/login");
+      }
+    }, [isExist]);
+
+    return <WrappedComponent />;
+  };
+}
