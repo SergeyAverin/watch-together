9b90fefcfa955ac09ea74fd72901669954df1394
SergeyAverin <ct.averin@yandex.ru>
2024-03-18 10:40:08 +0600

    feat(frontend): added login page

---
+++ frontend/src/components/pages/LoginPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Center } from "@atoms/index";
+import { LoginForm } from "@organisms/LoginForm/LoginForm";
+
+
+const LoginPage: React.FC = () => {
+  return (
+    <Center>
+      <LoginForm />
+    </Center>
+  );
+};
+
+export default LoginPage;
