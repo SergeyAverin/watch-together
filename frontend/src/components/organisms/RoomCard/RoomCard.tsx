129dd6d3ffec047a2359256083c1d9b0bc61c971
SergeyAverin <ct.averin@yandex.ru>
2024-12-05 17:54:56 +0600

    feat(frontend): added room card

---
+++ frontend/src/components/organisms/RoomCard/RoomCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./RoomCard.sass";
+
+export const RoomCard: React.FC = () => {
+  return <div className="roomCard"></div>;
+};
--- frontend/src/components/pages/RoomsPage/RoomsPage.tsx
+++ frontend/src/components/pages/RoomsPage/RoomsPage.tsx
@@ -1,5 +1,23 @@
+import RoomCard from "@organisms/RoomCard";
 import React from "react";
+import "./RoomPage.sass";
+import { Typography, TypographyVariant } from "@atoms/index";
 
 export const RoomsPage: React.FC = () => {
-  return <></>;
+  return (
+    <div className="wrapper">
+      <Typography variant={TypographyVariant.H1}>Owned room</Typography>
+      <div className="roomCardGrid">
+        <RoomCard />
+        <RoomCard />
+        <RoomCard />
+      </div>
+      <Typography variant={TypographyVariant.H1}>Joined rooms</Typography>
+      <div className="roomCardGrid">
+        <RoomCard />
+        <RoomCard />
+        <RoomCard />
+      </div>
+    </div>
+  );
 };
